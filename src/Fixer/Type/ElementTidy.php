8fb6137e6c8514f7a69b7a0db9e4e89be989e0f4
Kei <budi03@gmail.com>
2026-01-12 14:02:24 +0700

    Add comments to regex match variables in ElementTidy
    Clarified the purpose of variables extracted from the regex match array in `applyFix` by adding inline comments describing their contents.

--- src/Fixer/Type/ElementTidy.php
+++ src/Fixer/Type/ElementTidy.php
@@ -22,11 +22,11 @@ final class ElementTidy
             return $line;
         }
 
-        $domainBlock = $m[1];
-        $modifier = $m[2] ?? ''; // AdGuard modifier
-        $domain = $m[3];
-        $separator = $m[4];
-        $selector = $m[5];
+        $domainBlock = $m[1];    // [$adg-modifiers]example.com
+        $modifier = $m[2] ?? ''; // [$adg-modifiers]
+        $domain = $m[3];         // example.com
+        $separator = $m[4];      // ##
+        $selector = $m[5];       // .ads
 
         if (str_starts_with($modifier, '[$') && $this->adgModifier->isComplicated($modifier)) {
             $modifier = $this->adgModifier->extract($domainBlock);
