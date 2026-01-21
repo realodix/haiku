678ede6c444cfe8a8211258b3fae68a40458cbb5
Kei <budi03@gmail.com>
2026-01-21 10:32:16 +0700

    add unit test to Builder

---
+++ tests/Unit/Builder/CleaningTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Realodix\Haiku\Test\Unit\Builder;
+
+use PHPUnit\Framework\Attributes as PHPUnit;
+use Realodix\Haiku\Builder\Cleaner;
+use Realodix\Haiku\Test\TestCase;
+
+class CleaningTest extends TestCase
+{
+    #[PHPUnit\Test]
+    public function agent(): void
+    {
+        $input = [
+            '[Adblock]',
+            '[AdBlock]',
+            '[Adblock Plus]',
+            '[Adblock Plus 2.0]',
+            '[AdGuard]',
+            '[uBlock]',
+            '[uBlock Origin]',
+            '[uBlock Origin Lite]',
+            '[uBO Lite]',
+        ];
+        $this->assertSame([], Cleaner::clean($input, false));
+
+        // not agent
+        $input = [
+            '[$adg-modifier]##[class^="ads-"]',
+            '[$adg-modifier]$$script[data-src="banner"]',
+        ];
+        $this->assertSame($input, Cleaner::clean($input, false));
+    }
+
+    #[PHPUnit\Test]
+    public function comment(): void
+    {
+        $input = [
+            '! comment',
+            '# comment',
+            '#comment',
+        ];
+
+        $this->assertSame([], Cleaner::clean($input, false));
+    }
+
+    #[PHPUnit\Test]
+    public function blank(): void
+    {
+        $input = [
+            '      ',
+            '',
+        ];
+
+        $this->assertSame([], Cleaner::clean($input, false));
+    }
+
+    #[PHPUnit\Test]
+    public function unique(): void
+    {
+        $input = [
+            '##.ads',
+            '##.ads',
+        ];
+
+        $this->assertSame(['##.ads'], Cleaner::clean($input, true));
+    }
+}
