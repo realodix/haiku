755624ae089e4570ff343138f4d2075d405addae
Kei <budi03@gmail.com>
2026-09-11 02:05:37 +0700

    Release 1.13.20

--- src/App.php
+++ src/App.php
@@ -11,7 +11,7 @@ use Illuminate\Container\Container;
 class App
 {
     const NAME = 'Haiku';
-    const VERSION = '1.13.19-dev';
+    const VERSION = '1.13.20';
 
     public static function version(): string
     {
