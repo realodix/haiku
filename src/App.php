36f56e940b9d8ff77742fd89280d1a3c2c1ce6d3
Kei <budi03@gmail.com>
2026-09-17 11:40:24 +0700

    [ci skip] Reverting release version changes

--- src/App.php
+++ src/App.php
@@ -11,7 +11,7 @@ use Illuminate\Container\Container;
 class App
 {
     const NAME = 'Haiku';
-    const VERSION = '1.13.22';
+    const VERSION = '1.13.22-dev';
 
     public static function version(): string
     {
